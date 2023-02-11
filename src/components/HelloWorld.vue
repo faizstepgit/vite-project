0b4e2e0aac920a5e0b7bedbf69bb2c25b2c25fa2
Fight <82665258+faizstepgit@users.noreply.github.com>
2023-02-11 21:54:45 +0700

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -21,6 +21,7 @@
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 <script setup>
 import { ref } from 'vue'
